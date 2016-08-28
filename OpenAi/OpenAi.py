c9a64f21013e43404d4aa81473530bb7cb240bd0
Shupper <hooper.simon@gmail.com>
2016-08-28 23:45:32 +0100

    base of NN

--- OpenAi/OpenAi.py
+++ OpenAi/OpenAi.py
@@ -14,6 +14,26 @@ env = gym.make('CarRacing-v0')
 env.reset()
 for _ in range(10):
     #env.render()
-    env.step(env.action_space.sample()) # take a random action
+    observation, reward, done, info = env.step(env.action_space.sample())
+    ff=observation.flatten()
+    print env.action_space.sample()
+    print ("observation:",env.observation_space)
+    #env.step(env.action_space.sample()) # take a random action
 
+
+import numpy as np
+
+NCount = 30000
+HdCount = 5
+
+input = np.random.random((1,NCount))
+
+synapseWeights = np.random.random((NCount,HdCount))
+
+#print x
+#print y
+
+output = np.dot(x,y)
+
+print output
 
